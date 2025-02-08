d7a611438e96304ca3312a74b2ec4752629a7203
M-D-Nadeem <149703432+M-D-Nadeem@users.noreply.github.com>
2025-02-08 19:15:53 +0530

    first commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app=express();
 app.get("/",(req,res)=>{
     res.send("Hello World!!")
 })
-app.listen("3002",'0.0.0.0',()=>{
+app.listen(3002,'0.0.0.0',()=>{
     console.log("Your server is running on http://localhost:3002/");
     
 })
